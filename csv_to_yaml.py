dba258133fbb31d1d8e377d04c396391c9575455
tbersez <thomas.bersez@gmail.com>
2019-01-25 10:32:16 +0100

    update and test from atom

--- csv_to_yaml.py
+++ csv_to_yaml.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
+# This script allow easy generation of .yaml configuration files
+# used by the Snakefile
 
 import sys
 import csv
 
 tab = '    '
 
-reads_dir = input('Path to raw read directory (must finish with a "/" !) : ')
+reads_dir = input('Path to raw read directory : ')
+genome_dir = input('Path to the reference genome_dir :')
 threads = input('How many threads should be used : ')
-
 
 with open(sys.argv[1], 'r') as file:
     reader = csv.DictReader(file, delimiter=',', quotechar='"')
     with open("config.yaml", 'w') as yaml:
         yaml.write("RAW: " + reads_dir + "\n")
-        yaml.write("TRIMMED: trimmed_reads/\n")
-        yaml.write("THREADS: " + threads + "\n")
+        yaml.write("REF: " + genome_dir + "\n")
+        yaml.write("THREADS: " + threads + "\n\n")
         yaml.write("samples:\n")
-        #looping over row to create a sample object for each sample
         for row in reader:
             yaml.write(tab + row["filename"] + ": \n")
             yaml.write(2*tab + "name: " + row["filename"] + "\n")
+            yaml.write(2*tab + "tech: " + row["tech"] + "\n")
+            yaml.write(2*tab + "p/s: " + row["p/s"] + "\n")
             yaml.write(2*tab + "R1: " + reads_dir + row["filename"] + row["R1_ext"] + "\n")
-            yaml.write(2*tab + "R2: " + reads_dir + row["filename"] + row["R2_ext"] + "\n")
+            if(row["p/s"] == 'p'):
+                yaml.write(2*tab + "R2: " + reads_dir + row["filename"] + row["R2_ext"] + "\n")
             yaml.write(2*tab + "platform: " + row["platform"] + "\n")
             yaml.write(2*tab + "date: " + row["date(mm/dd/yy)"] + "\n")
