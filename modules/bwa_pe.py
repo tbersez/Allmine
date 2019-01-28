5b3c24096495a258a2509f77e0f7f8797d98db7c
tbersez <thomas.bersez@gmail.com>
2019-01-28 17:31:23 +0100

    protecting outputed bams

--- modules/bwa_pe.py
+++ modules/bwa_pe.py
@@ -15,10 +15,11 @@ rule run_bwa_paired :
         R1 = expand(config["TRIMMED"] + "{samples}_1_trim.fastq.gz", samples = config["samples"]),
         R2 = expand(config["TRIMMED"] + "{samples}_2_trim.fastq.gz", samples = config["samples"])
     output:
-        bam = expand(config["MAP"] + "{samples}_sorted.bam", samples = config["samples"])
+        bam = protected(expand(config["MAP"] + "{samples}_sorted.bam", samples = config["samples"]))
     params:
         idxbase = config["REF"] + config["GENOME"]
     message: "Mapping reads {input.R1} and {input.R2} to {params.idxbase} using BWA.\n"
+    #converting to bam, sorting and removing dupplicates in a single command!
     shell:
         """
         /home/aa/anaconda3/bin/bwa mem \
@@ -26,5 +27,6 @@ rule run_bwa_paired :
         {input.R1} \
         {input.R2} \
         | /usr/bin/samtools view -Sb - \
-        | /usr/bin/samtools sort -o {output.bam} -
+        | /usr/bin/samtools sort -o - \
+        | /usr/bin/samtools rmdup -S - {output.bam}
         """
--- modules/bwa_se.py
+++ modules/bwa_se.py
@@ -14,15 +14,17 @@ rule run_bwa_paired :
     input:
         R1 = expand(config["TRIMMED"] + "{samples}_1_trim.fastq.gz", samples = config["samples"])
     output:
-        bam = expand(config["MAP"] + "{samples}_sorted.bam", samples = config["samples"])
+        bam = protected(expand(config["MAP"] + "{samples}_sorted.bam", samples = config["samples"]))
     params:
         idxbase = config["REF"] + config["GENOME"]
     message: "Mapping reads {input.R1} to {params.idxbase} using BWA.\n"
+    #converting to bam, sorting and removing dupplicates in a single command!
     shell:
         """
         /home/aa/anaconda3/bin/bwa mem \
         {params.idxbase} \
         {input.R1} \
         | /usr/bin/samtools view -Sb - \
-        | /usr/bin/samtools sort -o {output.bam} -
+        | /usr/bin/samtools sort -o - \
+        | /usr/bin/samtools rmdup -s - {output.bam}
         """
