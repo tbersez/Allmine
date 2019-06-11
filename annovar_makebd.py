e364da7f87c89b38ebd70d2e0eedef7a509da62b
tbersez <thomas.bersez@gmail.com>
2019-06-11 15:03:25 +0200

    commit

--- annovar_makebd.py
+++ annovar_makebd.py
@@ -13,12 +13,12 @@ config = yaml.safe_load(open('config.yaml'))
 os.system('rm -rf avdb/')
 os.system('mkdir avdb')
 if config["ANO"].endswith(".gff"):
-    os.system('singularity exec -B '+ config["BIND"] + ' ' + config["CONT"] + \
+    os.system('singularity exec --bind '+ config["BIND"] + ' ' + config["CONT"] + \
     " gff3ToGenePred " + config["REF"] + config["ANO"] + " avdb/AV_refGene.txt")
 elif config["ANO"].endswith(".gtf"):
-    os.system('singularity exec -B '+ config["BIND"] + ' ' + config["CONT"] + \
+    os.system('singularity exec --bind '+ config["BIND"] + ' ' + config["CONT"] + \
     " gtfToGenePred " + config["REF"] + config["ANO"] + " avdb/AV_refGene.txt")
-os.system('singularity exec -B '+ config["BIND"] + ' ' + config["CONT"] + \
+os.system('singularity exec --bind '+ config["BIND"] + ' ' + config["CONT"] + \
 " perl /opt/annovar/retrieve_seq_from_fasta.pl --format refGene --seqfile " + \
 config["REF"] + config["GENOME"] + " avdb/AV_refGene.txt --out avdb/AV_refGeneMrna.fa")
 print('\n\n\33[32mDone! Database builded successfully.\33[0m\n')
